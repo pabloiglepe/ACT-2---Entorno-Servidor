d28eeb582aeb1e28f20be5c6475935f313346eeb
pabloiglepe <pabloiglep2@gmail.com>
2025-09-25 13:06:06 +0200

    Entrega 2

---
+++ src/Laboral/DatosNoCorrectosException.java
@@ -0,0 +1,7 @@
+package Laboral;
+
+public class DatosNoCorrectosException extends RuntimeException {
+    public DatosNoCorrectosException(String message) {
+        super(message);
+    }
+}
---
+++ src/Laboral/Empleado.java
@@ -0,0 +1,58 @@
+package Laboral;
+
+public class Empleado extends Persona {
+
+    // ATRIBUTOS
+    private int categoria;
+    public int anyos;
+
+    // CONSTRUCTORES
+    public Empleado(char sexo, String dni, String nombre, int anyos, int categoria) {
+        super(sexo, dni, nombre);
+
+        this.anyos = anyos;
+        if (this.anyos < 0) {
+            throw new DatosNoCorrectosException("Datos no correctos");
+        }
+
+        this.categoria = categoria;
+        if (this.categoria < 0 || this.categoria > 10) {
+            throw new DatosNoCorrectosException("Datos no correctos");
+        }
+    }
+
+    public Empleado(char sexo, String dni, String nombre) {
+        super(sexo, dni, nombre);
+        this.anyos = 0;
+        this.categoria = 1;
+    }
+
+    // METODOS
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    // IMPLEMENTO EL GETANYOS PARA LA FUNCION SUELDO DE EMPLEADOS
+    public int getAnyos() {
+        return anyos;
+    }
+
+    public void incrAnyo() {
+        this.anyos++;
+    }
+
+
+    public String Imprime() {
+        return "Empleado{" +
+                "categoria=" + categoria +
+                ", anyos=" + anyos +
+                ", sexo=" + sexo +
+                ", dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
